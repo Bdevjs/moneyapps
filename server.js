a0b987c3099866e7885df380d05773b5b1300f18
mehedihassansovon <mehedi15-10712@diu.edu.bd>
2021-11-22 16:24:26 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+
+const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extanded: false }))
+app.use(bodyParser.json())
+
+
+app.get('/',(req, res)=>{
+    res.json({
+        massage : 'Welcome to Our Application'
+    })
+})
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+    mongoose.connect('mongodb://loaclhost/money-management-app', 
+    { useNewUrlParser: true },
+    ()=>{
+        console.log('Database connected.....')
+    });
+})
